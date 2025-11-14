f065494ac66441b41e7b892fdc073605967d55f3
Daniel Covarrubias <dacovasan00@gmail.com>
2025-11-14 14:51:04 -0600

    comment

--- src/db/schemas/gender_worte.rs
+++ src/db/schemas/gender_worte.rs
@@ -1,20 +1,25 @@
 use chrono::{DateTime, Utc};
 
 pub const CREATE_STR_TABLE_GENDER_WORTE: &str = "
-CREATE TABLE IF NOT EXISTS gender_worte (
-    id                  INTEGER PRIMARY KEY AUTOINCREMENT,
-    gender              TEXT NOT NULL,                         
-    artikel             TEXT NOT NULL,
+    CREATE TABLE IF NOT EXISTS gender_worte (
+        id                  INTEGER PRIMARY KEY AUTOINCREMENT,
+        gender              TEXT NOT NULL,                         
+        artikel             TEXT NOT NULL,
 
-    -- Generic
-    created_at          TEXT DEFAULT CURRENT_TIMESTAMP,
-    deleted_at          TEXT
-)";
-
-pub const CREATE_STR_INDEX_GENDER_WORTE: &str = "
-CREATE INDEX IF NOT EXISTS idx_gender_worte_created_at ON gender_worte(created_at);
+        -- Generic
+        created_at          TEXT DEFAULT CURRENT_TIMESTAMP,
+        deleted_at          TEXT
+    )
 ";
 
+pub const CREATE_STR_INDEX_GENDER_WORTE: &str = "
+    CREATE INDEX IF NOT EXISTS idx_gender_worte_created_at ON gender_worte(created_at);
+";
+
+// 0 - Maskuline - der
+// 1 - Femenin - die
+// 2 - Neutrum - das
+// 3 - Plural - die
 #[derive(Debug, Clone)]
 pub struct GenderWorteSchema {
     pub id: i32,
--- src/db/schemas/niveau_worte.rs
+++ src/db/schemas/niveau_worte.rs
@@ -1,19 +1,26 @@
 use chrono::{DateTime, Utc};
 
 pub const CREATE_STR_TABLE_GENDER_WORTE: &str = "
-CREATE TABLE IF NOT EXISTS niveau_worte (
-    id                  INTEGER PRIMARY KEY AUTOINCREMENT,
-    niveau              TEXT NOT NULL,                         
+    CREATE TABLE IF NOT EXISTS niveau_worte (
+        id                  INTEGER PRIMARY KEY AUTOINCREMENT,
+        niveau              TEXT NOT NULL,                         
 
-    -- Generic
-    created_at          TEXT DEFAULT CURRENT_TIMESTAMP,
-    deleted_at          TEXT
-)";
-
-pub const CREATE_STR_INDEX_NIVEAU_WORTE: &str = "
-CREATE INDEX IF NOT EXISTS idx_niveau_worte_created_at ON niveau_worte(created_at);
+        -- Generic
+        created_at          TEXT DEFAULT CURRENT_TIMESTAMP,
+        deleted_at          TEXT
+    )
 ";
 
+pub const CREATE_STR_INDEX_NIVEAU_WORTE: &str = "
+    CREATE INDEX IF NOT EXISTS idx_niveau_worte_created_at ON niveau_worte(created_at);
+";
+
+// 0 - A1
+// 1 - A2
+// 2 - B1
+// 3 - B2
+// 4 - C1
+// 5 - C2
 #[derive(Debug, Clone)]
 pub struct NiveauWorteSchema {
     pub id: i32,
