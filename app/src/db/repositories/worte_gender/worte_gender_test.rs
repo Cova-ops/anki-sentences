83ced71425e96d90eb6ea8eeaaa21c5285885d39
Daniel Covarrubias <dacovasan00@gmail.com>
2026-01-07 01:26:52 -0600

    refactor worte_gender

--- app/src/db/repositories/worte_gender/worte_gender_test.rs
+++ app/src/db/repositories/worte_gender/worte_gender_test.rs
@@ -1,33 +1,37 @@
 #[cfg(test)]
 mod test_worte_gender_repo {
     use color_eyre::eyre::Result;
-    use rusqlite::Connection;
 
     use crate::test_utils::prelude::*;
 
     mod bulk_upsert {
-
         use super::*;
 
-        fn run_bulk_upsert_scenario<F>(insert_fn: F, sc: Scenario<NewWorteGenderSchema>)
-        where
-            F: Fn(&mut Connection, &[NewWorteGenderSchema]) -> Result<Vec<WorteGenderSchema>>,
-        {
-            let mut conn = setup_test_db().unwrap();
-
-            let res_1 = insert_fn(&mut conn, &sc.initial).expect("La inserción no debe fallar");
-            res_1.assert_eq_fields(&sc.initial);
-            insta::assert_debug_snapshot!("after_insert", res_1.snapshot());
-
-            let res_2 = insert_fn(&mut conn, &sc.update).expect("La inserción no debe fallar");
-            res_2.assert_eq_fields(&sc.update);
-            insta::assert_debug_snapshot!("after_update", res_2.snapshot());
-        }
-
         #[test]
-        fn test_bulk_insert_and_update() {
+        fn test_bulk_upsert() -> Result<()> {
+            let mut conn = setup_test_db()?;
+
+            let res = WorteGenderRepo::bulk_upsert(&mut conn, &[])?;
+            res.assert_eq_fields(&vec![]);
+            insta::assert_debug_snapshot!("[WorteGender::bulk_upsert] - empty", res.snapshot());
+
             let sc = scenario_worte_gender();
-            run_bulk_upsert_scenario(|conn, data| WorteGenderRepo::bulk_upsert(conn, data), sc);
+
+            let res = WorteGenderRepo::bulk_upsert(&mut conn, &sc.initial)?;
+            res.assert_eq_fields(&sc.initial);
+            insta::assert_debug_snapshot!(
+                "[WorteGender::bulk_upsert] - after insert",
+                res.snapshot()
+            );
+
+            let res = WorteGenderRepo::bulk_upsert(&mut conn, &sc.update)?;
+            res.assert_eq_fields(&sc.update);
+            insta::assert_debug_snapshot!(
+                "[WorteGender::bulk_upsert] - after update",
+                res.snapshot()
+            );
+
+            Ok(())
         }
     }
 }
