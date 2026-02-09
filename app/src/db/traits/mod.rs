c819db7aded8646be674e363e2d6b30a4aaf3545
Daniel Covarrubias <dacovasan00@gmail.com>
2026-02-08 22:16:16 -0600

    remove unnecesary trait

--- app/src/db/traits/mod.rs
+++ app/src/db/traits/mod.rs
@@ -1,15 +1,3 @@
-use crate::helpers::error_handler::DbError;
-
-// Models
-pub(in crate::db) trait FromDTO<T> {
-    type Error;
-
-    fn from_raw<'a>(raw: &'a T) -> Result<Self, Self::Error>;
-    fn from_vec_raw<'a, I>(vec: I) -> Result<Vec<Self>, Self::Error>
-    where
-        I: IntoIterator<Item = &'a T>;
-}
-
 // Schemas
 pub(in crate::db) trait FromSql {
     fn from_sql(r: &rusqlite::Row<'_>) -> Result<Self, rusqlite::Error>
